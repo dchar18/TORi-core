c6bb53405f5b1ebaf7a9677ee4938eaf08714af9
Damian Charczuk <damiancharczuk@Damians-Mini.attlocal.net>
2023-10-25 15:43:23 -0500

    initial commit: creating springboot base

---
+++ src/test/kotlin/com/dcharczuk/TORicore/ToRiCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dcharczuk.TORicore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToRiCoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
---
+++ target/classes/com/dcharczuk/TORicore/ToRiCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.dcharczuk.TORicore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToRiCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<ToRiCoreApplication>(*args)
+}
